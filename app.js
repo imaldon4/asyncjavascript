b4499d1e31241b02bdce53ce44ff0ae5c64f0782
Ike Maldonado <31159328+imaldon4@users.noreply.github.com>
2020-07-30 16:39:36 -0700

    callbacks

--- app.js
+++ app.js
@@ -2,13 +2,22 @@ console.log('Start');
 
 function loginUser(email, password, callback) {
     setTimeout(() => {
-        console.log('Now we have the data');
+        console.log('Now we have the data:');
         callback({ userEmail: email });
     }, 2000);
 }
 
+function getUserVideos(email, callback) {
+    setTimeout(() => {
+        callback(['video1', 'video2', 'video3']);
+    }, 1000);
+}
+
 const user = loginUser('ike@goomail.com', 123456, (user) => {
     console.log(user);
+    getUserVideos(user.userEmail, (videos) => {
+        console.log(videos);
+    })
 });
 
 console.log('Finish');
