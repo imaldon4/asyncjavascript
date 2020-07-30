762636eec247b8b31b66b45fe9e4e5e26dbfcf67
Ike Maldonado <31159328+imaldon4@users.noreply.github.com>
2020-07-30 16:47:29 -0700

    callback hell

--- app.js
+++ app.js
@@ -4,19 +4,28 @@ function loginUser(email, password, callback) {
     setTimeout(() => {
         console.log('Now we have the data:');
         callback({ userEmail: email });
-    }, 2000);
+    }, 3000);
 }
 
 function getUserVideos(email, callback) {
     setTimeout(() => {
-        callback(['video1', 'video2', 'video3']);
-    }, 1000);
+        callback(["video1", "video2", "video3"]);
+    }, 2000);
 }
 
-const user = loginUser('ike@goomail.com', 123456, (user) => {
+function videoDetails(video, callback) {
+    setTimeout(() => {
+        callback("title of the video");
+    }, 2000);
+}
+
+const user = loginUser("ike@goomail.com", 123456, user => {
     console.log(user);
-    getUserVideos(user.userEmail, (videos) => {
+    getUserVideos(user.userEmail, videos => {
         console.log(videos);
+        videoDetails(videos[0], title => {
+            console.log(title);
+        })
     })
 });
 
